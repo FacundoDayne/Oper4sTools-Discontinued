d0d099715718917e5e55c528f95db9fa5a5dadc5
Vayne S <rutzivdayne@gmail.com>
2023-11-17 11:19:00 +0800

    didnt work

--- PostureCheck/Program.cs
+++ PostureCheck/Program.cs
@@ -16,6 +16,13 @@ namespace PostureCheck
 		[STAThread]
 		static void Main()
 		{
+
+			// Set up a handler for the ConsoleCancelEventHandler
+			Console.CancelKeyPress += Console_CancelKeyPress;
+
+			Console.WriteLine("This will appear in the console.");
+
+			// Allocate the console
 			AllocConsole();
 			Application.EnableVisualStyles();
 			Application.SetCompatibleTextRenderingDefault(false);
@@ -24,10 +31,36 @@ namespace PostureCheck
 			FreeConsole();
 		}
 
+		private static void Console_CancelKeyPress(object sender, ConsoleCancelEventArgs e)
+		{
+			e.Cancel = true; // Prevent the default behavior (terminate the application)
+			HideConsole();   // Instead, hide the console
+		}
+
+		private static void HideConsole()
+		{
+			IntPtr hwndConsole = GetConsoleWindow();
+
+			// If the console window is open, minimize it
+			if (hwndConsole != IntPtr.Zero)
+			{
+				ShowWindow(hwndConsole, SW_MINIMIZE);
+			}
+		}
+
+		// Constants for ShowWindow method
+		private const int SW_MINIMIZE = 6;
+
 		[DllImport("kernel32.dll")]
 		private static extern bool AllocConsole();
 
 		[DllImport("kernel32.dll")]
 		private static extern bool FreeConsole();
+
+		[DllImport("kernel32.dll")]
+		private static extern IntPtr GetConsoleWindow();
+
+		[DllImport("user32.dll")]
+		private static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
 	}
 }
